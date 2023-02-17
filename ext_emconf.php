b568261550cd1c10ee9af065e5d8eae10e098e21
Sven Roloff <post@wirkwerk.com>
2023-02-17 20:31:50 +0100

    * changed description in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,14 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'Clubmanager Base',
-	'description' => 'Clubmanager Base - The free TYPO3 module for managing clubs, foundations and associations. The only thing you need are members!',
+	'description' => 'The TYPO3 module for managing clubs, foundations and associations. With Clubmanager, you get the tool to manage the organization AND public relations of your club or association in a cost-saving and effective way.',
 	'category' => 'misc',
 	'author' => 'wirkwerk.com und codemacher.de',
 	'author_email' => 'post@quicko.software',
 	'author_company' => 'Quicko - Der Clubmanager',
 	'state' => 'stable',
 	'clearCacheOnLoad' => 1,
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => [
 		'depends' => [
 			'typo3' => '11.5.0-11.5.99',
