5ccd0414436847a3a6d8501c5f27a638d7ba8abf
Thomas Rokohl <thomas.rokohl@codemacher.de>
2024-01-10 16:41:16 +0100

    wip php typing

--- Classes/Controller/CitiesController.php
+++ Classes/Controller/CitiesController.php
@@ -3,36 +3,32 @@
 namespace Quicko\Clubmanager\Controller;
 
 use Psr\Http\Message\ResponseInterface;
-
-use Quicko\Clubmanager\Controller\BaseSettingsController;
 use Quicko\Clubmanager\Domain\Repository\LocationRepository;
 
 class CitiesController extends BaseSettingsController
 {
-  protected $locationRepository;
-
-  public function __construct(LocationRepository $locationRepository)
+  public function __construct(protected LocationRepository $locationRepository)
   {
-    $this->locationRepository = $locationRepository;
   }
 
-  public function listAction() : ResponseInterface
+  public function listAction(): ResponseInterface
   {
     $this->setDefaultSettingsIfRequired();
 
     $cities = $this->locationRepository->findCities();
-        
+
     $this->view->assign('cities', $cities);
+
     return $this->htmlResponse();
   }
 
-  public function detailAction(string $city) : ResponseInterface
+  public function detailAction(string $city): ResponseInterface
   {
     $this->setDefaultSettingsIfRequired();
     $locations = $this->locationRepository->findByCity($city);
     $this->view->assign('city', $city);
     $this->view->assign('locations', $locations);
+
     return $this->htmlResponse();
   }
- 
 }
