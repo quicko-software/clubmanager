aa581d9a6a2963fe9d1cb91e86c0727dedac99fc
Stephan Weigelt <stephan.weigelt@codemacher.de>
2024-05-22 10:29:54 +0200

    #5540, BUG: password recovery mail did not work, REF: rename classes, extract classes

---
+++ Classes/Utils/HookUtils.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Quicko\Clubmanager\Utils;
+
+use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\CMS\Core\Log\Logger;
+
+
+class HookUtils
+{
+  public static function getRecordUid(string $uidCandidate, DataHandler &$dataHandler): ?string
+  {
+    if (str_starts_with($uidCandidate, 'NEW')) {
+      return array_key_exists($uidCandidate, $dataHandler->substNEWwithIDs)
+        ? $dataHandler->substNEWwithIDs[$uidCandidate]
+        : null
+      ;
+    }
+    $trimmedUidCandidate = trim($uidCandidate);
+    return empty($trimmedUidCandidate)
+      ? null
+      : $trimmedUidCandidate
+    ;
+  }
+
+  public static function logError(Logger $logger, string $tableName, string $uid) {
+    $logger->error(
+      sprintf('Failed to process datamap for command (table=%s, uid=%s)',
+        $tableName,
+        $uid
+      )
+    );
+  }
+}
