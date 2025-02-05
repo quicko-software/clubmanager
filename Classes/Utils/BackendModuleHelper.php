72fcdb1aa4611825d66431ed4e5bb212c5c3567f
Thomas Rokohl <thomas.rokohl@codemacher.de>
2025-02-05 18:10:13 +0100

    cleanup

--- Classes/Utils/BackendModuleHelper.php
+++ Classes/Utils/BackendModuleHelper.php
@@ -3,26 +3,9 @@
 namespace Quicko\Clubmanager\Utils;
 
 use Quicko\Clubmanager\Controller\Backend\AdvertisingController;
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 class BackendModuleHelper
 {
-  public static function removeAdertisingModule(string $moduleName): void
-  {
-    $key = 'Clubmanager' . ucfirst($moduleName);
-    $fullkey = 'clubmanager_' . $key;
-
-    unset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['Clubmanager']['modules'][$fullkey]);
-    unset($GLOBALS['TBE_MODULES']['_configuration'][$fullkey]);
-
-    $clubmanager = $GLOBALS['TBE_MODULES']['clubmanager'];
-    $clubmanagerEntries = explode(',', $clubmanager);
-    if (($pos = array_search($key, $clubmanagerEntries)) !== false) {
-      unset($clubmanagerEntries[$pos]);
-    }
-    $GLOBALS['TBE_MODULES']['clubmanager'] = implode(',', $clubmanagerEntries);
-  }
-
   public static function createAdvertisingModuleDescripterV12(
     string $moduleName,
     string $beUrlSubPath
