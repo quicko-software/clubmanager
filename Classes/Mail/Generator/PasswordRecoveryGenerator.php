6f3a0d921b9ad1a527a8a5ce924c27a02302af5a
Thomas Rokohl <thomas.rokohl@codemacher.de>
2025-08-05 17:29:17 +0200

    bugfix/workaround password mails form backend

--- Classes/Mail/Generator/PasswordRecoveryGenerator.php
+++ Classes/Mail/Generator/PasswordRecoveryGenerator.php
@@ -2,11 +2,21 @@
 
 namespace Quicko\Clubmanager\Mail\Generator;
 
+<<<<<<< Updated upstream
+=======
+use Quicko\Clubmanager\Mail\Generator\Arguments\PasswordRecoveryArguments;
+use Quicko\Clubmanager\Records\FeUserRecordRepository;
+use Quicko\Clubmanager\Utils\ForgotPasswordHashGenerator;
+use Quicko\Clubmanager\Utils\TypoScriptUtils;
+use Quicko\Mailjournal\Mail\Generator\Arguments\BaseMailGeneratorArguments;
+>>>>>>> Stashed changes
 use Symfony\Component\Mime\Address;
-use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Crypto\HashService;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Mail\FluidEmail;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Site\SiteFinder;
+<<<<<<< Updated upstream
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Crypto\Random;
 use TYPO3\CMS\Extbase\Security\Cryptography\HashService;
@@ -16,21 +26,23 @@ use Quicko\Clubmanager\Mail\Generator\Arguments\BaseMailGeneratorArguments;
 use Quicko\Clubmanager\Mail\Generator\Arguments\PasswordRecoveryArguments;
 use Quicko\Clubmanager\Records\FeUserRecordRepository;
 use Quicko\Clubmanager\Utils\TypoScriptUtils;
+=======
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+use TYPO3\CMS\FrontendLogin\Controller\PasswordRecoveryController;
+>>>>>>> Stashed changes
 
 class PasswordRecoveryGenerator extends BaseMemberUidMailGenerator
 {
-
   private ?int $timestamp = null;
-  
+
   public function getLabel(BaseMailGeneratorArguments $args): string
   {
     return LocalizationUtility::translate('passwordrecoverygenerator.label', 'clubmanager') ?? '';
   }
 
   /**
-   * Returns TTL timestamp of the forgot hash
-   *
-   * @return int
+   * Returns TTL timestamp of the forgot hash.
    */
   public function getLifeTimeTimestamp(int $passwordRecoveryLifeTime): int
   {
@@ -39,41 +51,42 @@ class PasswordRecoveryGenerator extends BaseMemberUidMailGenerator
       $currentTimestamp = $context->getPropertyFromAspect('date', 'timestamp');
       $this->timestamp = $currentTimestamp + 3600 * $passwordRecoveryLifeTime;
     }
+
     return $this->timestamp;
   }
 
-  /**
-   * 
-   */
   protected function generateForgotHash(int $feUserUid, int $passwordRecoveryLifeTime): string
   {
-    $random = GeneralUtility::makeInstance(Random::class);
-    $hashService = GeneralUtility::makeInstance(HashService::class);
-    $randomString = $random->generateRandomHexString(16);
-    $forgotHash = $this->getLifeTimeTimestamp($passwordRecoveryLifeTime) . '|' . $hashService->generateHmac($randomString);
+    $forgotPasswordHashGenerator = GeneralUtility::makeInstance(ForgotPasswordHashGenerator::class);
+    $forgotHash = $forgotPasswordHashGenerator->generate();
+    $hmac = '';
+    if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() < 13) {
+      $hmac = GeneralUtility::hmac($forgotHash);
+    } else {
+      $hashService = GeneralUtility::makeInstance(HashService::class);
+      $hmac = $hashService->hmac($forgotHash, PasswordRecoveryController::class);
+    }
 
-
-    $feUserRecordRepo =  GeneralUtility::makeInstance(FeUserRecordRepository::class);
+    $feUserRecordRepo = GeneralUtility::makeInstance(FeUserRecordRepository::class);
     $feUserRecordRepo->update([$feUserUid], [
-      "felogin_forgotHash" => GeneralUtility::hmac($forgotHash)
+      'felogin_forgotHash' => $hmac,
     ]);
 
     return $forgotHash;
   }
 
-
   public function generateFluidMail(BaseMailGeneratorArguments $args): ?FluidEmail
   {
-
     /** @var PasswordRecoveryArguments $passwordArgs */
     $passwordArgs = $args;
 
     $repo = $this->getMemberRepo();
     $member = $repo->findByUid($passwordArgs->memberUid ?? 0);
-    if(!$member) return null;
+    if (!$member) {
+      return null;
+    }
     $address_email = $member['fe_users_email'];
-    $address_name = ($member['firstname'] ?? "") . ' ' . ($member['lastname'] ?? "");
-
+    $address_name = ($member['firstname'] ?? '') . ' ' . ($member['lastname'] ?? '');
 
     $loginPidString = TypoScriptUtils::getTypoScriptValueForPage('plugin.tx_clubmanager.settings.feUsersLoginPid', $member['fe_users_pid']);
     $loginPid = intval($loginPidString);
@@ -102,6 +115,7 @@ class PasswordRecoveryGenerator extends BaseMemberUidMailGenerator
   {
     $parameters = [];
     $site = GeneralUtility::makeInstance(SiteFinder::class)->getSiteByPageId($loginPid);
+
     return (string) $site->getRouter()->generateUri($loginPid, $parameters);
   }
 
@@ -114,8 +128,8 @@ class PasswordRecoveryGenerator extends BaseMemberUidMailGenerator
       'tx_felogin_login' => [
         'action' => 'showChangePassword',
         'controller' => 'PasswordRecovery',
-        'hash' => $forgotHash
-      ]
+        'hash' => $forgotHash,
+      ],
     ];
 
     return (string) $site->getRouter()->generateUri(
