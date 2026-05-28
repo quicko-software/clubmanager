9483bf0339704388d8dfd66d088d0ebbfbac5fe3
Thomas Rokohl <thomas.rokohl@codemacher.de>
2026-05-28 12:32:19 +0200

    Refactor LocationLatLngUpdateHook

--- Classes/Hooks/LocationLatLngUpdateHook.php
+++ Classes/Hooks/LocationLatLngUpdateHook.php
@@ -1,6 +1,7 @@
 <?php
 namespace Quicko\Clubmanager\Hooks;
 
+use Quicko\Clubmanager\Service\LocationCoordinatesUpdateService;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -16,49 +17,6 @@ class LocationLatLngUpdateHook
     }
 
 
-    private function updateLatLng(int $uid, string $lat, string $lng): void
-    {
-        $updateCommand = [];
-        $updateCommand['tx_clubmanager_domain_model_location'][$uid]['latitude'] =  $lat;
-        $updateCommand['tx_clubmanager_domain_model_location'][$uid]['longitude'] = $lng;
-
-        $dataHandler = $this->getDataHandler();
-        $dataHandler->start($updateCommand, []);
-        $commandResult = $dataHandler->process_datamap();
-        /*if ($commandResult === false) {
-            $this->logger->error(
-                sprintf(
-                    'Failed to process datamap for command on tx_clubmanager_domain_model_location (%d)',
-                    $uid,
-                    ['command' => $updateCommand]
-                )
-            );
-       }*/
-    }
-
-    protected function getCoordinates(string $address): mixed
-    {
-        $url = 'https://nominatim.openstreetmap.org/search?q=' . urlencode($address) . '&format=json&limit=1';
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 3);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Clubmanager');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/json']);
-        $response = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-    
-        if ($httpCode < 300 && $response && $response !== true) {
-            $json = json_decode($response, true);
-            if (count( $json) > 0 && $json[0]) {
-                return $json[0];
-            }
-        }
-    
-        return null;
-    }
 
     /**
      * @param string      $status
@@ -82,17 +40,11 @@ class LocationLatLngUpdateHook
             $uid,
             '*',
         );
-        $lat = $record['latitude'] ?? '';
-        $lng = $record['longitude'] ?? '';
 
-        if (empty($lat) || empty($lng)) {
-            $zip = $record["zip"] ?? '' ;
-            $city = $record["city"] ?? '';
-            $street = $record["street"] ?? '';
-            $loc = $this->getCoordinates("$zip $city, $street");
-            if($loc) {
-                $this->updateLatLng($uid,$loc["lat"],$loc["lon"]);
-            } 
+        $coordinatesUpdateService = GeneralUtility::makeInstance(LocationCoordinatesUpdateService::class);
+        if($coordinatesUpdateService->needsUpdate($record,$fieldArray)) {
+            $coordinatesUpdateService->updateCoordinates($uid);
         }
+        
     }
 }
